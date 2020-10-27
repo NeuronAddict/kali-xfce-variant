870b05ca7a5e0ea329d2be0e378c1840d467409f
neuronaddict <manufrommars@elacourarie.org>
2020-10-28 00:05:11 +0100

    Initial commit.

---
+++ docker/build-custom.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git
+cd live-build-config || exit 1
+cp -r /variant-xfce-custom kali-config/variant-xfce-custom
+
+./build.sh --variant xfce-custom --verbose
+
