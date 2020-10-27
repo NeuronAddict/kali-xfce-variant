c3e416f4ec3ed978bb85c343e1edf9a621ed8be4
neuronaddict <manufrommars@elacourarie.org>
2020-10-28 00:16:27 +0100

    Fix variant path

--- docker/build-custom.sh
+++ docker/build-custom.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git
 cd live-build-config || exit 1
 cp -r /variant-xfce-custom kali-config/variant-xfce-custom
