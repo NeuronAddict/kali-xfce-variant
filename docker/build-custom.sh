903fc5b1c2a8acbd43629962add3637a08b29e3a
neuronaddict <dev@lacourarie.org>
2020-10-28 22:57:25 +0100

    Support partial folder.

--- docker/build-custom.sh
+++ docker/build-custom.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git
-cd live-build-config || exit 1
+if [[ -d live-build-config ]]
+then
+  cd live-build-config || exit 1
+  git pull origin master
+else
+  git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git --single-branch -b master
+  ls -al
+  echo $PWD
+  cd live-build-config || exit 1
+fi
+
 cp -r /variant-xfce-custom kali-config/variant-xfce-custom
 
 
