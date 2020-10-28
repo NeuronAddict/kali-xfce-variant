275eb89841277ba36ed6c354b239506744670059
neuronaddict <dev@lacourarie.org>
2020-10-28 22:37:35 +0100

    Fix sys mount error.

--- docker/build-custom.sh
+++ docker/build-custom.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
-
 git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git
 cd live-build-config || exit 1
 cp -r /variant-xfce-custom kali-config/variant-xfce-custom
 
-ls -al
+
 bash -x ./build.sh --variant xfce-custom --verbose
 
+ls -al images
+cp images/*.iso /output
